efb8e6f7b6a7f74ba7f382d294e8e2b2c8f0d6ed
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-01-31 00:58:45 +0600

    In S2 extra Task2 completed

---
+++ S2-Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int X = new Random().Next(0,8);
+Console.WriteLine("X = " + X);
+
+if (X < 6)
+{
+    Console.WriteLine("будний день");
+}
+else
+{
+    Console.WriteLine("выходной день");
+}
