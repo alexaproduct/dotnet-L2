446af89ba1d15950a77700f6f0868e209eda263e
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 15:09:08 +0600

    S3 task corrected version

---
+++ S3-Task3-Upd/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+Console.WriteLine("Введите число X");
+int X = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число Y");
+int Y = int.Parse(Console.ReadLine());
+
+if (X > 0 && Y > 0) Console.WriteLine("Четверть I");
+
+else if (X < 0 && Y > 0) Console.WriteLine("Четверть II");
+
+else if (X < 0 && Y < 0) Console.WriteLine("Четверть III");
+
+else if (X == 0 || Y == 0) Console.WriteLine("Введите числа > или < 0");
+
+else Console.WriteLine("Четверть IV");
