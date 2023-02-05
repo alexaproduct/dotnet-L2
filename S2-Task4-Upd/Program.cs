7ba72324145a9935409a64ef5a88f78e95c9e153
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-05 15:38:28 +0600

    Task 4 Version 2

---
+++ S2-Task4-Upd/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task 4 alt version
+
+
+Console.Write("Ведите число: ");
+int number = int.Parse(Console.ReadLine());
+int mod = 0;
+int newmod = 0;
+int i=0;
+for(i= number;number!=0;number=number/10)
+{
+mod = number % 10;
+newmod = newmod*10 + mod;
+}
+if(i == newmod) Console.WriteLine("Число палиндром");
+else Console.WriteLine("число не является палиндромом");
